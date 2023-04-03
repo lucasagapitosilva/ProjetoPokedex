8c136ba121652fbb3cc4080715f7f59170935ea8
lucas <lucasbezerra.rocinha@gmail.com>
2023-04-03 18:24:12 -0300

    Mostrando imagens, e alternando paginas

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,17 @@
 import './style.css';
+import { useNavigate } from 'react-router-dom';
 
 import { SiPokemon } from 'react-icons/si';
 
 
-export default function Header() {
+export default function Header({ handleTitleClick }) {
+
+
  return (
    <header>
     <SiPokemon size={80} color="#FFF" className='logo'/>
     <div className='topo'>
-      <h1>Pokédex</h1>
+      <h1 onClick={handleTitleClick}>Pokédex</h1>
     </div>
    </header>
  );
--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,8 +1,57 @@
 import './style.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { FaSearch } from 'react-icons/fa';
+import api from '../../Services/api';
+
 
 export default function Main() {
+
+  const navigate = useNavigate();
+
+  const [pokeData, setPokeData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0)
+
+  useEffect(() => {
+    async function loadApi() {
+      setLoading(true)
+      const offset = page * 6;
+      const response = await api.get(`pokemon?limit=6&offset=${offset}`)
+
+      const PokemonInfo = response.data.results
+
+      setPokeData(PokemonInfo);
+      setLoading(false)
+      console.log(PokemonInfo)
+
+    }
+
+    loadApi();
+  }, [page])
+
+
+  function handleNextClick() {
+    setPage(page + 1);
+  }
+
+  function handlePrevClick() {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  }
+
+  function handleTitleClick(){
+    navigate('/')
+  }
+
+
+  if (loading) {
+    return <p className='loading'>Loading...</p>
+  }
+
+
   return (
     <div className='container-geral'>
       <div className='container-search'>
@@ -19,43 +68,35 @@ export default function Main() {
       <div className='container'>
 
         <div className='container-cards'>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
-          <div className='card'>
-            <p>nº001</p>
-            <img />
-            <span>Charizard</span>
-          </div>
+          {pokeData.map((pokemon, index) => {
+            const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.url.split('/')[6]}.svg`;
+            return (
+              <div key={pokemon.name} className='card'>
+                <p>{page * 6 + index + 1 < 10 ? `nº00${page * 6 + index + 1}` : `nº0${page * 6 + index + 1}`}</p>
+                <img src={url} alt={pokemon.name} />
+                <span>{pokemon.name}</span>
+              </div>
+            )
+          })}
         </div>
 
       </div>
 
       <div className='btns'>
-        <button type='button' onClick={() =>{}}>Voltar</button>
-        <button type='button' onClick={() =>{}}>Avançar</button>
+        {page <= 0 ? (
+          <button type="button" onClick={handleNextClick}>
+            Avançar
+          </button>
+        ) : (
+          <>
+            <button type="button" onClick={handlePrevClick}>
+              Voltar
+            </button>
+            <button type="button" onClick={handleNextClick}>
+              Avançar
+            </button>
+          </>
+        )}
       </div>
       <span className='dev'>Desenvolvedor - Lucas Agapito</span>
     </div>
